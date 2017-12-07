d954a64a6cfde57af8be35dec9ed009c38c35638
Danieldbc <daniel.dbc01@gmail.com>
2017-12-07 21:20:50 +0100

    Version 2.06

--- app/src/main/java/edu/eseiaat/upc/pma/manuel/daniel/zombicidesuport/JuegoActivity.java
+++ app/src/main/java/edu/eseiaat/upc/pma/manuel/daniel/zombicidesuport/JuegoActivity.java
@@ -1,11 +1,13 @@
 package edu.eseiaat.upc.pma.manuel.daniel.zombicidesuport;
 
 import android.graphics.drawable.Drawable;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -15,7 +17,8 @@ public class JuegoActivity extends AppCompatActivity {
 
     public static String KeyListaPersonajes="key_listaPersonajes";
     private int[] union;
-    private TextView habAzul,habAmarilla, habNaranja1, habNaranja2, habRoja1, habRoja2,habRoja3;
+    private TextView habAzul,habAmarilla, habNaranja1, habNaranja2, habRoja1, habRoja2,habRoja3,nombre;
+    private ImageView foto;
     private ArrayList<Personaje> listaPersonajes;
     private ArrayList<Personaje> listaPersonajesSelec;
     private RecyclerView viewPersonajes;
@@ -34,6 +37,8 @@ public class JuegoActivity extends AppCompatActivity {
         habRoja1=(TextView) findViewById(R.id.HabRoja1);
         habRoja2=(TextView) findViewById(R.id.HabRoja2);
         habRoja3=(TextView) findViewById(R.id.HabRoja3);
+        foto=(ImageView)findViewById(R.id.foto);
+        nombre=(TextView)findViewById(R.id.nombre);
 
         union=getIntent().getIntArrayExtra(KeyListaPersonajes);
         listaPersonajes=new ArrayList<>();
@@ -80,6 +85,8 @@ public class JuegoActivity extends AppCompatActivity {
         habRoja1.setText(p.getHabRoja1());
         habRoja2.setText(p.getHabRoja2());
         habRoja3.setText(p.getHabRoja3());
+        foto.setImageDrawable(p.getCara());
+        nombre.setText(p.getNombre());
         if (!p.level[0]){
             habAmarilla.setBackgroundColor(getColor(android.R.color.white));
         }

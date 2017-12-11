443a345015e8a3836b88b7acf01957a62a664211
Danieldbc <Danieldbc@github.com>
2017-12-11 12:45:05 +0100

    Version 2.11

--- app/src/main/java/edu/eseiaat/upc/pma/manuel/daniel/zombicidesuport/CardsActivity.java
+++ app/src/main/java/edu/eseiaat/upc/pma/manuel/daniel/zombicidesuport/CardsActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import java.util.ArrayList;
 
@@ -22,7 +23,7 @@ public class CardsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cards);
         listacartas=new ArrayList<>();
-        Todas();
+        Crear();
         viewCartas=(RecyclerView)findViewById(R.id.ViewCartas);
         linlayoutmanager =new LinearLayoutManager(this);
         viewCartas.setLayoutManager(linlayoutmanager);
@@ -35,20 +36,24 @@ public class CardsActivity extends AppCompatActivity {
         icono.setImageDrawable(getDrawable(R.drawable.calavera));*/
     }
 
-    private void Todas() {
-        listacartas.clear();
-        listacartas.add(new Cartas(getDrawable(R.drawable.cbagofrice)));
-        listacartas.add(new Cartas(getDrawable(R.drawable.ccannedfood)));
-        listacartas.add(new Cartas(getDrawable(R.drawable.cwater)));
-        listacartas.add(new Cartas(getDrawable(R.drawable.cgasoline)));
-        listacartas.add(new Cartas(getDrawable(R.drawable.cglassbottle)));
-        listacartas.add(new Cartas(getDrawable(R.drawable.cgoaliemask)));
-        listacartas.add(new Cartas(getDrawable(R.drawable.cflashlight)));
-        listacartas.add(new Cartas(getDrawable(R.drawable.cplentyofammo)));
-        listacartas.add(new Cartas(getDrawable(R.drawable.cplentyofammoshotgun)));
-        listacartas.add(new Cartas(getDrawable(R.drawable.cscope)));
-        listacartas.add(new Cartas(getDrawable(R.drawable.cmolotov)));
+    private void Crear() {
+        OtrosCartas();
+        EspecialesCartas();
+        CuerpoCartas();
+        DistanciaCartas();
+    }
 
+    private void DistanciaCartas() {
+        listacartas.add(new Cartas(getDrawable(R.drawable.cmashotgun)));
+        listacartas.add(new Cartas(getDrawable(R.drawable.ceviltwins)));
+        listacartas.add(new Cartas(getDrawable(R.drawable.cpistol)));
+        listacartas.add(new Cartas(getDrawable(R.drawable.crifle)));
+        listacartas.add(new Cartas(getDrawable(R.drawable.csawedoff)));
+        listacartas.add(new Cartas(getDrawable(R.drawable.cshotgun)));
+        listacartas.add(new Cartas(getDrawable(R.drawable.csubmg)));
+    }
+
+    private void CuerpoCartas() {
         listacartas.add(new Cartas(getDrawable(R.drawable.cbaseballbat)));
         listacartas.add(new Cartas(getDrawable(R.drawable.cchainsaw)));
         listacartas.add(new Cartas(getDrawable(R.drawable.ccrowbar)));
@@ -56,15 +61,59 @@ public class CardsActivity extends AppCompatActivity {
         listacartas.add(new Cartas(getDrawable(R.drawable.ckatana)));
         listacartas.add(new Cartas(getDrawable(R.drawable.cmachete)));
         listacartas.add(new Cartas(getDrawable(R.drawable.cpan)));
-
         listacartas.add(new Cartas(getDrawable(R.drawable.cmashotgun)));
+    }
 
-        listacartas.add(new Cartas(getDrawable(R.drawable.ceviltwins)));
-        listacartas.add(new Cartas(getDrawable(R.drawable.cpistol)));
-        listacartas.add(new Cartas(getDrawable(R.drawable.crifle)));
-        listacartas.add(new Cartas(getDrawable(R.drawable.csawedoff)));
-        listacartas.add(new Cartas(getDrawable(R.drawable.cshotgun)));
-        listacartas.add(new Cartas(getDrawable(R.drawable.csubmg)));
+    private void EspecialesCartas() {
+        listacartas.add(new Cartas(getDrawable(R.drawable.cgoaliemask)));
+        listacartas.add(new Cartas(getDrawable(R.drawable.cflashlight)));
+        listacartas.add(new Cartas(getDrawable(R.drawable.cplentyofammo)));
+        listacartas.add(new Cartas(getDrawable(R.drawable.cplentyofammoshotgun)));
+        listacartas.add(new Cartas(getDrawable(R.drawable.cscope)));
+        listacartas.add(new Cartas(getDrawable(R.drawable.cmolotov)));
+    }
 
+    private void OtrosCartas() {
+        listacartas.add(new Cartas(getDrawable(R.drawable.cbagofrice)));
+        listacartas.add(new Cartas(getDrawable(R.drawable.ccannedfood)));
+        listacartas.add(new Cartas(getDrawable(R.drawable.cwater)));
+        listacartas.add(new Cartas(getDrawable(R.drawable.cgasoline)));
+        listacartas.add(new Cartas(getDrawable(R.drawable.cglassbottle)));
+    }
+
+    public void Todas(View view) {
+        listacartas.clear();
+        Crear();
+        adaptercartas.notifyDataSetChanged();
+        viewCartas.smoothScrollToPosition(0);
+
+    }
+
+    public void Distancia(View view) {
+        listacartas.clear();
+        DistanciaCartas();
+        adaptercartas.notifyDataSetChanged();
+        viewCartas.smoothScrollToPosition(0);
+    }
+
+    public void Cuerpo(View view) {
+        listacartas.clear();
+        CuerpoCartas();
+        adaptercartas.notifyDataSetChanged();
+        viewCartas.smoothScrollToPosition(0);
+    }
+
+    public void Especiales(View view) {
+        listacartas.clear();
+        EspecialesCartas();
+        adaptercartas.notifyDataSetChanged();
+        viewCartas.smoothScrollToPosition(0);
+    }
+
+    public void Otros(View view) {
+        listacartas.clear();
+        OtrosCartas();
+        adaptercartas.notifyDataSetChanged();
+        viewCartas.smoothScrollToPosition(0);
     }
 }
